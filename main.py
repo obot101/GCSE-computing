0d0e40543e0f625e07cdc05fb61bb8340f3eed50
oisin <112019174+obot101@users.noreply.github.com>
2024-09-14 12:05:17 +0000

    f

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ name = user_name.split(" ")
 user_input = input(f"Hi, {name[-1]} how are you?").upper()
 
 user_feeling = user_input.upper().split("I AM ")
+
+user_answer = input(f"Why do you feel{user_feeling[-1].lower()}")
