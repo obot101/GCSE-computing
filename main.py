64c96c980a35a29c1128cd52d5e9dce5fde0fec1
oisin <112019174+obot101@users.noreply.github.com>
2024-09-14 12:01:05 +0000

    commit 1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-wow this is nice
+user_name = input("Hi, I am Rasputin, what's yours?")
+
+name = user_name.split(" ")
+
+user_input = input(f"Hi, {name[-1]} how are you?").upper()
+
+user_feeling = user_input.upper().split("I AM ")
