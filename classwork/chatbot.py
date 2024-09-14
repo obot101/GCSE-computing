36d36e2afa76fca9a43c140db35b80dd975f7eb8
oisin <112019174+obot101@users.noreply.github.com>
2024-09-14 21:15:27 +0000

    f

--- classwork/chatbot.py
+++ classwork/chatbot.py
@@ -1,11 +1,14 @@
-main():
+import string
+def main():
     user_name = input("Hi, I am Rasputin, what's yours?")
 
     name = user_name.split(" ")
 
-    user_input = input(f"Hi, {name[-1]} how are you?").upper()
+    user_input = user_input((f"Hi, {name[-1]} how are you?").upper())
 
     user_feeling = user_input.upper().split("I AM ")
 
-    user_answer = input(f"Why do you feel {user_feeling[-1].lower()}?")
+    user_answer = user_input((f"Why do you feel {user_feeling[-1].lower()}?"))
 
+def remove_punction(inputD):
+    return inputD.translate(str.maketrans('', '', string.punctuation))
