dd29c1ab261a6597addcf05af6dc627ec88b05f3
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:22:58 +0000

    s

--- classwork/stringTypes.py
+++ classwork/stringTypes.py
@@ -3,4 +3,4 @@ string_end = ""
 print(carnival[0:5], end=string_end)
 print(carnival[5:8], end=string_end)
 print(f"{carnival[8:]} ", end=string_end)
-print(f"{carnival[:-1]} ", end=string_end)
+print(f"{carnival[:-0]} ", end=string_end)
