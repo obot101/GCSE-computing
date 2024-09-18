d346dfc73fe0c71993b16433e4a73c9d435b3eef
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:36:08 +0000

    s

--- classwork/stringMalipulation.py
+++ classwork/stringMalipulation.py
@@ -1,3 +1,4 @@
+
 carnival = "This is incredible"
 carnival2 = "0"
 string_end = ""
@@ -5,4 +6,4 @@ print(carnival[0:5], end=string_end)
 print(carnival[5:8], end=string_end)
 print(f"{carnival[8:]} ", end=string_end)
 print(f"{carnival[::2]} ", end=string_end)
- print(f"car")
+print(f"{carnival + carnival2}")
