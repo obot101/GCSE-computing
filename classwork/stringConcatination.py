7a1fafe612d324bb5511192135973de907e08294
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:49:47 +0000

    s

--- classwork/stringConcatination.py
+++ classwork/stringConcatination.py
@@ -2,6 +2,6 @@ a = 10
 b = 20
 
 print(a, b)
-print("a=", a,"b=", b)
-print("a= " + str(a) + " b= " + str(b))
+print("a = ", a,"b =", b)
+print("a = " + str(a) + " b = " + str(b))
 print(f"a = {a}, b = {b}")
