75eca050f3417b27bba80bdf6f550055b1647f9b
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:58:15 +0000

    g

--- classwork/stringConcatination.py
+++ classwork/stringConcatination.py
@@ -1,7 +1,11 @@
+#assign variables
 a = 10
 b = 20
-
+#print just a and b
 print(a, b)
+#print a and b seperately
 print("a = ", a,"b =", b)
+#print a and b after converting to strings
 print("a = " + str(a) + " b = " + str(b))
+#print using format string
 print(f"a = {a}, b = {b}")
