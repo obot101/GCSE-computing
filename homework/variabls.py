b9e47848097d71230b8ef3a7623ffbf68e760b46
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:13:15 +0000

    s

--- homework/variabls.py
+++ homework/variabls.py
@@ -1,4 +1,4 @@
 howManySweets = int(input("How many sweets: "))
 howManyKids = int(input("How many kids?"))
 
-print(f"Each child has {howManySweets // howManyKids}, so there there will be {howManySweets % howManyKids}")
+print(f"Each child has {howManySweets // howManyKids} sweets, so there there will be {howManySweets % howManyKids} sweets left")
