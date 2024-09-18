c6e84d77894e0326098db1817854088db9b6e5b3
oisin <112019174+obot101@users.noreply.github.com>
2024-09-18 09:20:57 +0000

    s

--- classwork/stringTypes.py
+++ classwork/stringTypes.py
@@ -1,5 +1,5 @@
 carnival = "This is incredible"
 string_end = ""
 print(carnival[0:5], end=string_end)
-print(carnival[5:7], end=string_end)
+print(carnival[5:8], end=string_end)
 print(carnival[8:], end=string_end)
