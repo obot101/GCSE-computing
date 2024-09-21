205f20e80b08d2619c822776c786eff1351f238b
oisin <112019174+obot101@users.noreply.github.com>
2024-09-21 18:52:10 +0000

    ss

--- homework/homework2.py
+++ homework/homework2.py
@@ -1,6 +1,6 @@
 treeDigit = int(input("Please enter a 3 digit nunber"))
 
 print(treeDigit // 100)
-lastTwo = (treeDigit % 100) // 10
+lastTwo = (treeDigit % 100)
 print(lastTwo // 10)
 print(treeDigit % 10)
