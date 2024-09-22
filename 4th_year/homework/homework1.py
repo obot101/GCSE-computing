f820d02751172206fe9cfb8eb7760bce3c0da8a2
oisin <112019174+obot101@users.noreply.github.com>
2024-09-22 11:55:21 +0100

    Rename variabls.py to homework1.py

---
+++ 4th_year/homework/homework1.py
@@ -0,0 +1,4 @@
+howManySweets = int(input("How many sweets: "))
+howManyKids = int(input("How many kids?"))
+
+print(f"Each child has {howManySweets // howManyKids} sweets, so there there will be {howManySweets % howManyKids} sweets left")
--- 4th_year/homework/variabls.py
+++
@@ -1,4 +0,0 @@
-howManySweets = int(input("How many sweets: "))
-howManyKids = int(input("How many kids?"))
-
-print(f"Each child has {howManySweets // howManyKids} sweets, so there there will be {howManySweets % howManyKids} sweets left")
