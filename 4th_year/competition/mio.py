40406093d6f629e8cb222ee4e55dc104adadd259
oisin <112019174+obot101@users.noreply.github.com>
2024-12-06 12:30:22 +0000

    dd

--- 4th_year/competition/mio.py
+++ 4th_year/competition/mio.py
@@ -5,7 +5,7 @@ while True:
     n += 1
     whole_string = whole_string + str(n)
     if len(whole_string) > i:
-        print(whole_string[i])
+        print(whole_string[i-1])
         break
     else:
         continue
