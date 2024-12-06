192502510605085f14292dfa7dfe20cd8444573a
oisin <112019174+obot101@users.noreply.github.com>
2024-12-06 11:32:53 +0000

    sd

--- 4th_year/competition/mio.py
+++ 4th_year/competition/mio.py
@@ -1,10 +1,11 @@
 n = int(input("Start of string:"))
 i = int(input("Position in string:"))
-q = 0
-whole_string = str(n)
 
-for i in range(2):
-    n = n+1
-    whole_string = whole_string + str(n)
-    
-print(whole_string)
+
+while True:
+    n += 1
+    if n == i:
+        print(n)
+        break
+    else:
+        continue
