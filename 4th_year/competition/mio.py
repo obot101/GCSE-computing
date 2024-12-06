a43160b510916c790a8862ca33ec2905dab18cc3
oisin <112019174+obot101@users.noreply.github.com>
2024-12-06 12:23:40 +0000

    s

--- 4th_year/competition/mio.py
+++ 4th_year/competition/mio.py
@@ -1,9 +1,11 @@
 n = int(input("Start of string: "))
 i = int(input("Position in string: "))
-
-if n < i:
-    while n < i:
-        n += 1
-    print(n)
-else:
-    print(f"Starting value ({n}) is already greater than or equal to the position ({i}).")
+whole_string = str(n)
+while True:
+    n += 1
+    whole_string = whole_string + str(n)
+    if len(n) >= i:
+        print(whole_string[i])
+        break
+    else:
+        continue
