459106ad3d57c8d8e0e7fd818e3479a6a666d8b5
oisin <112019174+obot101@users.noreply.github.com>
2025-01-22 15:07:55 +0000

    asdd

--- 4th_year/classwork/challenge.py
+++ 4th_year/classwork/challenge.py
@@ -2,5 +2,10 @@ import time
 print("Please enter 7 numbers:")
 temperatures = []
 for i in range(7):
-    temp = input)
-    temperatures.append("")
+    temp = int(input(f"Enter day {i + 1}: "))
+    temperatures.append(temp)
+average = 1
+for i in temperatures:
+    average = average * temperatures[i]
+
+print(average)
