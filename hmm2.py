1e8f4255c8691346c34b34fbb8510197f227506d
oisin <112019174+obot101@users.noreply.github.com>
2025-01-31 12:50:07 +0000

    362626

---
+++ hmm2.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+matches=["cider", "split", "cola", "juice", "slices", "balls", "chutney"]
+
+for i in range(len(fruits)):
+  print(fruits[i])
