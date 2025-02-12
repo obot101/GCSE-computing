91c7c1c83bf84d2770d5d7a3f1d2742f81b7a7bc
oisin <112019174+obot101@users.noreply.github.com>
2025-02-12 10:15:07 +0000

    sdd

---
+++ 4th_year/classwork/challenges.py
@@ -0,0 +1,12 @@
+events = [
+  ["05/02/2023", "WS2", "Window", "38"],
+  ["05/02/2023", "MS1", "Motion", "2"],
+  ["06/02/2023", "DS3", "Door", "1"],
+  ["06/02/2023", "MS2", "Motion", "3"],
+  ["06/02/2023", "MS1", "Motion", "2"],
+  ["07/02/2023", "WS1", "Window", "24"],
+  ["07/02/2023", "DS1", "Door", "1"]
+]
+userDate = input("Please enter a date:")
+for userDate in events:
+    print(events[0])
