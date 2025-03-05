5c8c1b2273e13ea82275a74adafa3cff9bde605b
oisin <112019174+obot101@users.noreply.github.com>
2025-03-05 10:07:49 +0000

    sdfsdf

--- 4th_year/classwork/challenges/challenge3.py
+++ 4th_year/classwork/challenges/challenge3.py
@@ -1,3 +1,3 @@
-width = input("What is the width(cm): ")
-length = input("What is the length(cm): ")
+width = int(input("What is the width(cm): "))
+length = int(input("What is the length(cm): "))
 print(f"{width * length}cm")
