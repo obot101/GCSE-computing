f3f02e4de4a9820a1ec1591d3f28d9678e36c895
oisin <112019174+obot101@users.noreply.github.com>
2025-04-30 14:09:44 +0000

    asdad

---
+++ 4th_year/classwork/linearsearch.py
@@ -0,0 +1,11 @@
+import sys
+Alist = [34,6,7,50,16,25,62,40]
+number = input("Enter a number: ")
+for i in Alist:
+    if i[Alist] == number:
+        print("Found it!")
+        sys.exit()
+    else:
+        continue
+print("Not in list!")
+    
